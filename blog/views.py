86762d1ad78e2491d6433a63e6091589252ae508
arunbikka <70753866+arunbikka@users.noreply.github.com>
2020-10-11 16:14:51 +0530

    updated

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,14 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'arun kumar',
-        'title': 'python project',
-        'posted' : 'august'
-    },
-    {
-        'author': 'arun',
-        'title': 'python ',
-        'posted' : 'august'
-    }
-]
+from .models import Post
 
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html',{'title':'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
